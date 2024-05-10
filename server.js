e6541aceeffdb8bc222c9c57457110ee12494688
sarveshwar <sarveshwar2005@gmail.com>
2024-05-10 12:42:36 +0530

    changes

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var path = require('path');
 
+app.set('view engine','ejs')
+
+app.use(express.static(path.join(__dirname,'public')))
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index',{test:'hi'})
 })
 
 app.listen(port, () => {
